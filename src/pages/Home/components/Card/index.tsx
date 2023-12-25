ed8b94386c614f9b924ef06c6b6d8cfede302209
marcossnikel <marcosnikeldev@gmail.com>
2023-12-25 20:35:46 -0300

    💄 : improve component readability

--- src/pages/Home/components/Card/index.tsx
+++ src/pages/Home/components/Card/index.tsx
@@ -1,31 +1,26 @@
 import * as S from './styles';
-import data  from '../../../../../data.json'
-export function Card() {
+import { Item, useCartStore } from '../../../../store/CartStore';
 
+type CardProps = {
+  coffe: Item;
+};
+
+export function Card({ coffe }: CardProps) {
+  const addToCart = useCartStore((state )=> state.addToCart)
   return (
-    <S.Container>
-      <h1>Nossos Cafés</h1>
-      <S.ProductsListContainer>
-        {data.coffees.map((coffe) => {
-          return (
-            <S.ProductItem key={coffe.id}>
-              <img src={coffe.image} alt="" />
-              <S.TagsContainer>
-                {coffe.tags.map(tag => {
-                  return <span key={tag}>{tag}</span>;
-                })}
-              </S.TagsContainer>
-              <S.ProductTitle>{coffe.title}</S.ProductTitle>
-              <S.ProductDescription>{coffe.description}</S.ProductDescription>
-              <S.StyledFooterProductItem>
-                <S.ProductItemPrice>R$ {coffe.price.toFixed(2)}</S.ProductItemPrice>
-
-                <S.StyledAddToCartButton size={30} />
-              </S.StyledFooterProductItem>
-            </S.ProductItem>
-          );
-        })}
-      </S.ProductsListContainer>
-    </S.Container>
+        <S.CardContainer>
+          <img src={coffe.image} alt="" />
+          <S.TagsContainer>
+            {coffe.tags.map(tag => {
+              return <span key={tag}>{tag}</span>;
+            })}
+          </S.TagsContainer>
+          <S.CardTitle>{coffe.title}</S.CardTitle>
+          <S.CardDescription>{coffe.description}</S.CardDescription>
+          <S.CardFooter>
+            <S.CardPrice>R$ {coffe.price.toFixed(2)}</S.CardPrice>
+            <S.StyledAddToCartButton size={30} onClick={() => addToCart(coffe)}/>
+          </S.CardFooter>
+        </S.CardContainer>
   );
 }
