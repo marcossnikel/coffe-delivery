e48ca8b85ad60395ffe30b626b85e16feb8d1d40
marcossnikel <marcosnikeldev@gmail.com>
2023-12-27 07:10:55 -0300

    fix 🐛 : add href into header links

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,17 +1,22 @@
+import { Link } from 'react-router-dom';
 import * as S from './styles';
 import logo from '/images/logo.svg';
 import { MapPin, ShoppingCart } from 'phosphor-react';
+import { useCartStore } from '../../store/CartStore';
 export function Header() {
+  const cart = useCartStore((state) => state.cart)
   return (
     <S.HeaderContainer>
-      <img src={logo} alt="Logo" />
+      <a href='/'><img src={logo} alt="Logo"/></a>
       <S.Container>
         <S.Location>
           <MapPin size={20} style={{ color: '#8047F8' }} />
           <p>Jundiaí, SP</p>
         </S.Location>
         <S.ShoppingCartContainer>
+          <Link to={'checkout'} aria-disabled={cart.length === 0}>
           <ShoppingCart size={20} />
+          </Link>
         </S.ShoppingCartContainer>
       </S.Container>
     </S.HeaderContainer>
